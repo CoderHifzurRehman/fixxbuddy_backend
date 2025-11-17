143a24b159428d00bb3b7ba412a22e94837165b4
CoderHifzurRehman <hifzurrehman8199@gmail.com>
2025-11-17 21:53:44 +0530

    revert 2 bck changes

--- src/config/email.js
+++ src/config/email.js
@@ -2,17 +2,14 @@ const nodemailer = require('nodemailer')
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-  // service: process.env.EMAIL_SERVICE,
+  service: process.env.EMAIL_SERVICE,
   host: process.env.EMAIL_HOST,
-  port: 465,
-  secure: true,
+  port:process.env.EMAIL_PORT,
+  secure: false,
   auth:{
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   },
-  tls: {
-    rejectUnauthorized: false
-  }
 });
 
 const sendEmail = async(subject, recipientEmail, body) =>{
