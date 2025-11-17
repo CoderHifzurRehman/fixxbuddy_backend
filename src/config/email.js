75f092846fbc808beb9bae6f9c96c36a5afa8cf1
CoderHifzurRehman <hifzurrehman8199@gmail.com>
2025-11-17 21:33:13 +0530

    fixx2

--- src/config/email.js
+++ src/config/email.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 const transporter = nodemailer.createTransport({
   // service: process.env.EMAIL_SERVICE,
   host: process.env.EMAIL_HOST,
-  port: Number(process.env.EMAIL_PORT),
-  secure: false,
+  port: 465,
+  secure: true,
   auth:{
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
